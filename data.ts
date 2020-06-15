c8571cc5050d2a00503d7331dd1aeebb96850046
Paul DAO <dao.van.paul@gmail.com>
2020-06-15 18:30:36 +0200

    create express server

---
+++ data.ts
@@ -0,0 +1,51 @@
+const data =  {
+    products : [
+        {
+            id: 1,
+            name: 'Viet Nam',
+            category: 'Asia',
+            image: '/images/vietnam.jpg',
+            price: 2499,
+            rating: 4.5,
+            nbReviews: 10
+        },
+        {
+            id: 2,
+            name: 'Marocco',
+            category: 'Africa',
+            image: '/images/marocco.jpg',
+            price: 299,
+            rating: 4,
+            nbReviews: 21
+        },
+        {
+            id: 3,
+            name: 'Spain',
+            category: 'Europe',
+            image: '/images/spain.jpg',
+            price: 399,
+            rating: 4.5,
+            nbReviews: 32
+        },
+        {
+            id: 4,
+            name: 'Paris',
+            category: 'Europe',
+            image: '/images/paris.jpg',
+            price: 249,
+            rating: 3.5,
+            nbReviews: 18
+        },
+        {
+            id: 5,
+            name: 'New-York',
+            category: 'North America',
+            image: '/images/new-york.jpg',
+            price: 2999,
+            rating: 4.5,
+            nbReviews: 52
+        },
+    ]
+}
+
+export default data;
