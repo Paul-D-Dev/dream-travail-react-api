dd4b3dd34f864dede1139e89950e2cc948f6f063
Paul DAO <dao.van.paul@gmail.com>
2020-07-14 11:20:00 +0200

    feat : create signin and register routes user with jwt

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export default {
-    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/dream'
+    MONGODB_URL : process.env.MONGODB_URL || 'mongodb://localhost/dream',
+    JWT_SECRET: process.env.JWT_SECRET || 'secret'
 } 
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,55 @@
 import express from 'express';
 import User from '../models/userModel';
+import { getToken } from '../util';
 
 const router = express.Router();
+
+router.post('/register', async (req, res) => {
+
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        const newUser = await user.save();
+    
+        if (newUser) {
+            res.send({
+                _id: signinUser.id,
+                name: signinUser.name,
+                email: signinUser.email,
+                isAdmin : signinUser.isAdmin,
+                token: getToken(signinUser)
+            })
+        } else {
+            res.status(401).send({msg: 'Invalid email or password.'})
+        }
+    } catch (error) {
+        res.send({msg: error.message})
+    }
+    
+}) 
+
+router.post('/signin', async (req, res) => {
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    if (signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin : signinUser.isAdmin,
+            token: getToken(signinUser)
+        })
+    } else {
+        res.status(401).send({msg: 'Invalid email or password.'})
+    }
+}) 
 
 router.get('/createadmin', async (req, res) => {
 
